2bb3454fce4a5dac2c9660819cc30b9214b7cfb6
DGarciaMedina <43907632+DGarciaMedina@users.noreply.github.com>
2018-10-20 18:44:07 +0100

    created matrix and dictionary for pathways and genes relationship

--- data_analysis.py
+++ data_analysis.py
@@ -4,9 +4,11 @@ import csv
 import matplotlib.pyplot as plt
 from bioservices.kegg import KEGG
 
+np.set_printoptions(threshold=np.nan)
+
 data = []
 
-df = pd.read_csv('RPKMs.csv',delimiter=";") 
+df = pd.read_csv('RPKMs.csv',delimiter=",") 
 
 k = KEGG()
 
@@ -16,16 +18,43 @@ k = KEGG()
     
     
 def search_pathways_4_list(list_of_genes):
+    
+    matrix = [[0 for j in range(len(list_of_genes))] for i in range(0)]
+    list_of_pathways = []
+    dict_of_genes = {}
+    
     for i,gene in enumerate(list_of_genes):
         try:
-            print(i,"****")
-            print("//\n",k.get_pathway_by_gene(gene, "hsa"))
+            pathways = k.get_pathway_by_gene(gene, "hsa")
+            
+            if pathways != None:
+                pathways = pathways.values()
+                
+                dict_of_genes[gene] = list(pathways)
+            
+                for pathway in pathways:
+                    
+                    if pathway not in list_of_pathways:
+                        list_of_pathways.append(pathway)
+                        
+                        matrix.append([0 for j in range(len(list_of_genes))])
+                        
+                        
+                    matrix[list_of_pathways.index(pathway)][i] = 1
+            
+                
         except Exception as e:
             print(e)
+            
+    return matrix,list_of_pathways,dict_of_genes
 
 
-search_pathways_4_list(df["symbol"][:100])
+list_of_genes = df["symbol"][:10]
 
+matrix, list_of_pathways, dict_of_genes = search_pathways_4_list(list_of_genes)
+
+print(np.array(matrix))
+print(dict_of_genes)
 
 
  
